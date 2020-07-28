07c1c286fcfb67269abf1dc9ddacfe0ac69e9b5a
Russell Esperanza <resperanza11@gmail.com>
2020-07-27 20:35:47 -0700

    created api endpoints

---
+++ endpoints.js
@@ -0,0 +1,61 @@
+export default Endpoints = {
+  animals: {
+    title: 'Animals',
+    url: 'https://opentdb.com/api.php?amount=1&category=27&difficulty=easy',
+  },
+
+  anime: {
+    title: 'Anime & Manga',
+    url: 'https://opentdb.com/api.php?amount=1&category=31&difficulty=easy',
+  },
+
+  cartoons: {
+    title: 'Cartoons & Animations',
+    url: 'https://opentdb.com/api.php?amount=1&category=32&difficulty=easy',
+  },
+
+  celebrities: {
+    title: 'Celebrities',
+    url: 'https://opentdb.com/api.php?amount=1&category=26&difficulty=easy',
+  },
+
+  computers: {
+    title: 'Computers',
+    url: 'https://opentdb.com/api.php?amount=1&category=18&difficulty=easy',
+  },
+
+  film: {
+    title: 'Film',
+    url: 'https://opentdb.com/api.php?amount=1&category=11&difficulty=easy',
+  },
+
+  general: {
+    title: 'General Knowledge',
+    url: 'https://opentdb.com/api.php?amount=1&category=9&difficulty=easy',
+  },
+
+  geography: {
+    title: 'Geography',
+    url: 'https://opentdb.com/api.php?amount=1&category=22&difficulty=easy',
+  },
+
+  history: {
+    title: 'History',
+    url: 'https://opentdb.com/api.php?amount=1&category=23&difficulty=easy',
+  },
+
+  music: {
+    title: 'Music',
+    url: 'https://opentdb.com/api.php?amount=1&category=12&difficulty=easy',
+  },
+
+  sports: {
+    title: 'Sports',
+    url: 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy',
+  },
+
+  television: {
+    title: 'Television',
+    url: 'https://opentdb.com/api.php?amount=1&category=14&difficulty=easy',
+  }
+}
