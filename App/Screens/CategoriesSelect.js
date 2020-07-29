19ee553e1ea786402cbce18ad1d248a413eae8c8
Koboh Kosaka <kobohkosaka@outlook.com>
2020-07-28 23:22:05 -0700

    Refactor to class component, create modal to present description and play button to forward to Game view.

--- App/Screens/CategoriesSelect.js
+++ App/Screens/CategoriesSelect.js
@@ -1,42 +1,142 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, Button, Modal} from 'react-native';
 
-export default function CategoriesSelect(props) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={{fontSize: 30}}>Select A Category!</Text>
+
+export default class CategoriesSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      modalVisible: true,
+      currentCat: '',
+    }
+    this.setModalVisible = this.setModalVisible.bind(this)
+  }
+
+  setModalVisible = (visible, e) => {
+    this.setState({
+      modalVisible: visible,
+      currentCat: e,
+    });
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+
+        <Modal visible={this.state.modalVisible}>
+          <View style={styles.modal}>
+            <Text style={styles.modalTitle}>{this.state.currentCat}</Text>
+            <Text style={styles.modalDescription}>this.props.categories.currentCat.description How about now Super long description that doesnt mean anything does it look ok? Or does it need to be fixed</Text>
+            <View style={styles.modalButtons}>
+              <TouchableHighlight  onPress={() => {this.setModalVisible(false)}}>
+                <Text style={styles.modalText}>Back</Text>
+              </TouchableHighlight>
+              <TouchableHighlight  onPress={() => {console.log(`Pass to the Game View ${this.state.currentCat}`)}}>
+                <Text style={styles.modalText}>Play</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+      <Text style={{fontSize: 35}}>Select A Category!</Text>
       <View style={styles.categories}>
-        {props.categories.map((cat) => {
+        {/* {props.categories.map((cat) => {
           return (
-            <TouchableHighlight style={styles.button} onPress={() => {console.log('Pressed')}}>
+            <TouchableHighlight style={styles.button} onPress={() => {this.setModalVisible(true, cat.title)}}>
               <Text style={{fontSize: 20}}>{cat.title}</Text>
             </TouchableHighlight>
           )
-        })}
+        })} */}
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category1')}}>
+            <Text style={{fontSize: 20}}>Category1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category2')}}>
+            <Text style={{fontSize: 20}}>Category2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category3')}}>
+            <Text style={{fontSize: 20}}>Category3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category4')}}>
+            <Text style={{fontSize: 20}}>Category4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category5')}}>
+            <Text style={{fontSize: 20}}>Category5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category6')}}>
+            <Text style={{fontSize: 20}}>Category6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category7')}}>
+            <Text style={{fontSize: 20}}>Category7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category8')}}>
+            <Text style={{fontSize: 20}}>Category8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category9')}}>
+            <Text style={{fontSize: 20}}>Category9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category10')}}>
+            <Text style={{fontSize: 20}}>Category10</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category11')}}>
+            <Text style={{fontSize: 20}}>Category11</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {this.setModalVisible(true, 'category12')}}>
+            <Text style={{fontSize: 20}}>Category12</Text>
+          </TouchableOpacity>
       </View>
     </SafeAreaView>
-  );
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    top: 30,
+    justifyContent: 'space-around',
   },
   categories: {
-    flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-evenly',
+    alignContent: 'center',
   },
   button: {
-    width: 150,
-    height: 70,
+    width: '40%',
+    height: '15%',
     margin: 10,
     borderRadius: 20,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'dodgerblue',
+  },
+  modal: {
+    top: '20%',
+    height: '50%',
+    margin: '10%',
+    borderRadius: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    alignContent: 'space-around',
+    backgroundColor: 'dodgerblue',
+  },
+  modalTitle: {
+    fontSize: 30,
+    justifyContent: 'center'
+  },
+  modalDescription: {
+    fontSize: 20,
+    width: '90%',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '70%',
+  },
+  modalText: {
+    fontSize: 25,
   }
 });
+
