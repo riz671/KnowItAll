09ba2be49240c817abbbb0336b74eb02d1d0667b
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-07-29 14:08:25 -0700

    add teamSelect component to file

---
+++ App/Screens/TeamSelect.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import StatsData from "./StatsViewComp/StatsData.js";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default class Stats_View extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+}
