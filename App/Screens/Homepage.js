d30099f26fc928ab43d8b330d084e952caa673c3
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-08-01 14:13:29 -0700

    center logo

--- App/Screens/Homepage.js
+++ App/Screens/Homepage.js
@@ -1,49 +1,57 @@
-import React from 'react';
-import { Image, StyleSheet, SafeAreaView, View, Text, TouchableHighlight } from 'react-native';
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableHighlight,
+  Animated,
+} from "react-native";
 
 const Homepage = ({ history }) => {
   return (
     <SafeAreaView style={styles.page}>
       <Text style={styles.title}>Know it all</Text>
       <View style={styles.page}>
-        <Image
-          source={require('../assets/splash.png')}
-          style={styles.logo}
-        />
+        <Image source={require("../assets/mainLogo.png")} style={styles.logo} />
       </View>
-      <TouchableHighlight 
-        style={styles.playNowButton} 
-        onPress={() => history.push('/team-select')}
+      <TouchableHighlight
+        style={styles.playNowButton}
+        onPress={() => history.push("/team-select")}
         activeOpacity={0.5}
-        underlayColor='#0065d1'
+        underlayColor="#0065d1"
       >
         <Text style={styles.playNow}>Play Now</Text>
       </TouchableHighlight>
     </SafeAreaView>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#383e4e',
-    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#383e4e",
+    width: "100%",
   },
   logo: {
-    height: 400,
-    width: 400,
-    marginBottom: 75
+    height: 250,
+    width: 250,
+    marginBottom: 40,
+    marginLeft: 30,
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
-    color: '#fff',
-    marginTop: 125, 
-    fontSize: 50 
+    color: "#fff",
+    marginTop: 125,
+    fontSize: 50,
   },
   playNow: {
-    color: '#fff',
-    fontSize: 25
+    color: "#fff",
+    fontSize: 25,
   },
   playNowButton: {
     backgroundColor: "#5a91e8",
@@ -51,10 +59,10 @@ const styles = StyleSheet.create({
     marginBottom: 200,
     fontSize: 100,
     height: 75,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 200
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+    width: 200,
+  },
+});
 
 export default Homepage;
